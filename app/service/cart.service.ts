66f98740bcd86ec508d16b82826de5d2c83b61cc
rdsemon <codewithemons@gmail.com>
2026-07-03 22:36:50 +0600

    feat(cartService): implement getCart and increaseQuantity function

--- app/service/cart.service.ts
+++ app/service/cart.service.ts
@@ -7,6 +7,22 @@ export const addToCart = async (cartData) => {
     console.log(response.data);
   } catch (error) {
     if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data?.message);
+    }
+  }
+};
+
+export const getCartItems = async () => {
+  const response = await api.get("/cart");
+  console.log(response.data);
+};
+
+export const increaseItemQuantity = async (cakeId: string) => {
+  try {
+    const response = await api.patch("/cart/increase", { cakeId });
+    console.log(response.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
       throw new Error(error.response?.data?.message || "add to cart fail");
     }
   }

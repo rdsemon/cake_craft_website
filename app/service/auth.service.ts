dbadb3b54151b164f71cae3e6a5bd642d7445383
rdsemon <codewithemons@gmail.com>
2026-07-05 23:19:59 +0600

    refactor(authService): change on types name froo lower to upper

--- app/service/auth.service.ts
+++ app/service/auth.service.ts
@@ -1,14 +1,14 @@
 import api from "./api";
 
-import type { userLogin, userSignUp } from "./types/auth.types";
+import type { UserLogin, UserSignUp } from "./types/auth.types";
 
-export const login = async (data: userLogin) => {
+export const login = async (data: UserLogin) => {
   const response = await api.post("/auth/login", data);
 
   return response.data;
 };
 
-export const signUp = async (data: userSignUp) => {
+export const signUp = async (data: UserSignUp) => {
   const response = await api.post("/auth/signUp", data);
   return response.data;
 };

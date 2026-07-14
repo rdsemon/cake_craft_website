15a80c67c2f4c9dfb6cd2250dd8a1cde837092cf
rdsemon <codewithemons@gmail.com>
2026-07-14 22:17:51 +0600

    refactor: change the cakeTypes path

--- app/service/cake.service.ts
+++ app/service/cake.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import api from "./api";
-import type { CakesResponse, Cake } from "./types/cake.types";
+import type { CakesResponse, Cake } from "../types/cake.types";
 
 export const getAllCakes = async (): Promise<CakesResponse> => {
   const response = await api.get<CakesResponse>("/cakes");
@@ -10,20 +10,17 @@ export const getAllCakes = async (): Promise<CakesResponse> => {
 export const getOneCake = async (id: string): Promise<Cake> => {
   try {
     const response = await api.get<Cake>(`/cakes/${id}`);
-
     console.log(response.data);
-
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       throw new Error(error.response?.data?.message || "Failed to fetch cake");
     }
-
     throw new Error("Unexpected error");
   }
 };
 
-export const createCake = async (cakeData:Cake) => {
+export const createCake = async (cakeData: Cake) => {
   try {
     const response = await api.post("/cakes", cakeData);
     console.log(response.data);

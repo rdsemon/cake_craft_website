2af41214734ade55bc41fb9e50b4afc33e8841e6
rdsemon <codewithemons@gmail.com>
2026-08-19 23:42:07 +0600

    refactor: use AuthInput reuseabel component

--- components/signUpForm.tsx
+++ components/signUpForm.tsx
@@ -1,65 +1,48 @@
 import { Lock, User, Mail } from "lucide-react";
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-} from "@/components/ui/input-group";
-import { Field, FieldLabel, FieldGroup } from "@/components/ui/field";
+import AuthInput from "./authInput";
+import { FieldGroup } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
 
 export default function SingUPForm() {
   return (
     <div>
       <form action="">
         <FieldGroup>
-          <Field>
-            <FieldLabel htmlFor="name">Full Name</FieldLabel>
-            <InputGroup>
-              <InputGroupAddon>
-                <User size={18} />
-              </InputGroupAddon>
-              <InputGroupInput id="name" type="text" placeholder="john doe" />
-            </InputGroup>
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="email">Email</FieldLabel>
-            <InputGroup>
-              <InputGroupAddon>
-                <Mail size={18} />
-              </InputGroupAddon>
-              <InputGroupInput
-                id="email"
-                type="email"
-                placeholder="youremail@gmail.com"
-              />
-            </InputGroup>
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="password">Password</FieldLabel>
-            <InputGroup>
-              <InputGroupAddon>
-                <Lock size={18} />
-              </InputGroupAddon>
-              <InputGroupInput
-                id="password"
-                type="password"
-                placeholder="*******"
-              />
-            </InputGroup>
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="confirmPassword">confirm password</FieldLabel>
-            <InputGroup>
-              <InputGroupAddon>
-                <Lock size={18} />
-              </InputGroupAddon>
-              <InputGroupInput
-                id="confirmPassword"
-                type="password"
-                placeholder="*******"
-              />
-            </InputGroup>
-          </Field>
+          <AuthInput
+            icon={<User size={18} />}
+            label="Full Name"
+            id="name"
+            type="text"
+            placeholder="john Doe"
+          />
+          <AuthInput
+            icon={<Mail size={18} />}
+            label="Email"
+            id="email"
+            type="email"
+            placeholder="your@gmail.com"
+          />
+          <AuthInput
+            icon={<Lock size={18} />}
+            label="Password"
+            id="password"
+            type="password"
+            placeholder="*******"
+          />
+          <AuthInput
+            icon={<Lock size={18} />}
+            label="Confirm Password"
+            id="password"
+            type="password"
+            placeholder="*******"
+          />
         </FieldGroup>
+        <Button
+          type="submit"
+          className="block mt-6 text-md w-full bg-primary text-primary-foreground font-medium rounded-lg hover:bg-primary/90 transition-colors uppercase"
+        >
+          Sign Up
+        </Button>
       </form>
     </div>
   );

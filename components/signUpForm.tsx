57d686ba304d9b2cc9ded1002899aaa516bd618a
rdsemon <codewithemons@gmail.com>
2026-08-27 22:42:07 +0600

    add react-hook-form

--- components/signUpForm.tsx
+++ components/signUpForm.tsx
@@ -1,12 +1,34 @@
+"use client";
 import { Lock, User, Mail } from "lucide-react";
 import AuthInput from "./authInput";
 import { FieldGroup } from "@/components/ui/field";
 import { Button } from "@/components/ui/button";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+interface Inputs {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export default function SingUPForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log("i got the click");
+    console.log(data);
+    console.log(errors);
+    reset();
+  };
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FieldGroup>
           <AuthInput
             icon={<User size={18} />}
@@ -14,6 +36,7 @@ export default function SingUPForm() {
             id="name"
             type="text"
             placeholder="john Doe"
+            registration={register("name", { required: true })}
           />
           <AuthInput
             icon={<Mail size={18} />}
@@ -21,6 +44,7 @@ export default function SingUPForm() {
             id="email"
             type="email"
             placeholder="your@gmail.com"
+            registration={register("email", { required: true })}
           />
           <AuthInput
             icon={<Lock size={18} />}
@@ -28,6 +52,7 @@ export default function SingUPForm() {
             id="password"
             type="password"
             placeholder="*******"
+            registration={register("password", { required: true })}
           />
           <AuthInput
             icon={<Lock size={18} />}
@@ -35,6 +60,7 @@ export default function SingUPForm() {
             id="password"
             type="password"
             placeholder="*******"
+            registration={register("confirmPassword", { required: true })}
           />
         </FieldGroup>
         <Button

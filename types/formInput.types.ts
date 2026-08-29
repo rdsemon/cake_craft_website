fa551c9a88bd3b97c1852b221f19f4461e1afcbc
rdsemon <codewithemons@gmail.com>
2026-08-29 21:51:44 +0600

    feat: write types for login and signUp form

---
+++ types/formInput.types.ts
@@ -0,0 +1,11 @@
+export interface SignUpFormInputs {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface LoginInputs {
+  email: string;
+  password: string;
+}

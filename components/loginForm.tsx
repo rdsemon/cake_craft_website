483ae5304786140a4b68eb04b911207e84f7d794
rdsemon <codewithemons@gmail.com>
2026-08-30 22:48:24 +0600

    feat: implement react-hook-form

--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,9 +1,38 @@
+"use client";
 import AuthInput from "./authInput";
 import { Mail, Lock } from "lucide-react";
 import { FieldGroup } from "./ui/field";
+import { useLoginMutation } from "@/services/authApi";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { LoginInputs } from "@/types/formInput.types";
+import FormButton from "./formButton";
+import { redirect } from "next/navigation";
+
 export default function LoginFrom() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<LoginInputs>();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const onSubmitForm = async (data: LoginInputs) => {
+    console.log(data);
+    try {
+      await login(data).unwrap();
+      toast.success("Login successful");
+      reset();
+      redirect("/");
+    } catch (error: any) {
+      toast.error(error.data.message || "someting went wrong");
+    }
+  };
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onSubmitForm)}>
       <FieldGroup>
         <AuthInput
           icon={<Mail size={18} />}
@@ -11,6 +40,10 @@ export default function LoginFrom() {
           id="email"
           type="email"
           placeholder="your@gmail.com"
+          registration={{
+            ...register("email", { required: "Email is required" }),
+          }}
+          error={errors.email?.message}
         />
         <AuthInput
           icon={<Lock size={18} />}
@@ -18,8 +51,13 @@ export default function LoginFrom() {
           id="password"
           type="password"
           placeholder="*******"
+          registration={{
+            ...register("password", { required: "Password is required" }),
+          }}
+          error={errors.password?.message}
         />
       </FieldGroup>
+      <FormButton isLoading={isLoading}>Login</FormButton>
     </form>
   );
 }

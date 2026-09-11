b53123e5d95b70000ddcf9473e1ee8eb7e3e2617
rdsemon <codewithemons@gmail.com>
2026-09-11 23:43:52 +0600

    refactor: define prop type and make it re-useable

--- components/passwordField.tsx
+++ components/passwordField.tsx
@@ -7,18 +7,32 @@ import {
 } from "@/components/ui/input-group";
 import { Field, FieldLabel, FieldError } from "@/components/ui/field";
 import { Eye, Lock, EyeOff } from "lucide-react";
+import type { UseFormRegisterReturn } from "react-hook-form";
 
-export default function PasswordField() {
+interface PassFieldProps {
+  label: string;
+  id: string;
+  registration: UseFormRegisterReturn;
+  error?: string;
+}
+
+export default function PasswordField({
+  label,
+  id,
+  registration,
+  error,
+}: PassFieldProps) {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <Field>
-      <FieldLabel htmlFor="password">Password</FieldLabel>
+      <FieldLabel htmlFor={id}>{label}</FieldLabel>
       <InputGroup>
         <InputGroupAddon>
           <Lock size={18} />
         </InputGroupAddon>
         <InputGroupInput
-          id="password"
+          id={id}
+          {...registration}
           type={showPassword ? "text" : "password"}
           placeholder="Enter your password"
         />
@@ -29,6 +43,7 @@ export default function PasswordField() {
           >
             {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
           </button>
+          {error && <FieldError>{error}</FieldError>}
         </InputGroupAddon>
       </InputGroup>
     </Field>
